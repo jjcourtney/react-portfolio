050c58f4e73b98d5dd26d38d10d39ad2dd1a8bed
jjcourtney <jcourtneywork@gmail.com>
2021-12-10 02:00:31 +0000

    added splash images and height config

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'splash1': "url('./images/splash1.jpg')",
+        'splash2': "url('./images/splash2.jpg')",
+      },
+      minHeight: {
+        '0': '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        'full': '100%',
+      }
+    },
   },
   variants: {
     extend: {},
