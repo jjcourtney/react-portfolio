6f62ae7a7709d4da37c661c55b73b5730712fbc4
jjcourtney <jcourtneywork@gmail.com>
2021-12-10 02:02:14 +0000

    created list of all projects

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,46 @@
+export const projectObjsArr = [
+    {
+        name: "infoCraft",
+        url: "https://jjcourtney.github.io/PDJ-InfoCraft/",
+        image: "../images/InfoCraft.png",
+        repoURL: "https://github.com/jjcourtney/PDJ-InfoCraft",
+        languages: ["JavaScript", "jQuery", "HTML", "Tailwind"],
+        isFeature: true
+    }, {
+        name: "weather-dash",
+        url: "https://jjcourtney.github.io/weather-dashboard/",
+        image: "../images/weather-dash.png",
+        repoURL: "https://github.com/jjcourtney/weather-dashboard",
+        languages: ["JavaScript", "HTML", "CSS"],
+        isFeature: false
+    }, {
+        name: "periodic-table",
+        url: "https://periodic.realalgorithm.co.uk/",
+        image: "../images/periodic.png",
+        repoURL: "https://github.com/jjcourtney/PeriodicTable",
+        languages: ["HTML", "PHP", "CSS"],
+        isFeature: false
+    }, {
+        name: "html-quiz",
+        url: "https://jjcourtney.github.io/Frontend-Developer-Quiz/",
+        image: "../images/html-quiz.png",
+        repoURL: "https://github.com/jjcourtney/Frontend-Developer-Quiz",
+        languages: ["JavaScript", "jQuery", "HTML", "CSS"],
+        isFeature: false
+    }, {
+        name: "daily-planner",
+        url: "https://jjcourtney.github.io/daily-planner/",
+        image: "../images/daily-planner.png",
+        repoURL: "https://github.com/jjcourtney/daily-planner",
+        languages: ["JavaScript", "jQuery", "HTML", "CSS"],
+        isFeature: false
+    }, {
+        name: "password-gen",
+        url: "https://jjcourtney.github.io/Password-Generator/",
+        image: "../images/password-gen.png",
+        repoURL: "https://github.com/jjcourtney/Password-Generator",
+        languages: ["JavaScript", "jQuery", "HTML", "CSS"],
+        isFeature: false
+    }]
+
+export const languages = ["HTML", "CSS", "JavaScript", "PHP", "Python", "Java", "jQuery", "Node", "Tailwind"];
