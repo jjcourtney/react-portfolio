4c06aadb6acf9283d062eebc673ad6758867baa0
jjcourtney <jcourtneywork@gmail.com>
2021-12-10 02:01:01 +0000

    created about page

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,21 @@
+
+
+function About() {
+    return (
+        <article className="w-screen flex justify-center bg-red-700 min-h-1/2">
+            <div className="flex justify-center max-w-5/6 border-2 border-red-700 rounded-md mx-24 bg-red-200 my-16">
+                <p className="font-serif text-3xl m-10">
+                    I am a full stack developer with commercial experience in node.js,
+                    Typescript and AWS (CDK infrastucture as code).
+                    I love problem solving and have a passion for development.
+                    I have been in people and project management for 17 years, and therefor
+                    bring a unique set of skills into the world of web development. A passion
+                    for everything code over the last 20 years has now driven me in to changing
+                    my career path into web development.
+                </p>
+            </div>
+        </article>
+    )
+}
+
+export default About;
