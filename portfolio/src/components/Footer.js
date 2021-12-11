a87e01456c4f744b03ebf5f0d8ffc97421299451
jjcourtney <jcourtneywork@gmail.com>
2021-12-11 00:28:06 +0000

    update css

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="w-screen bg-black">
             <div className="flex justify-around">
-                {links.map(link => <button className="inline-block bg-red-700 hover:bg-red-400 text-white font-bold py-2 px-4 rounded m-5"> {link} </button>)}
+                {links.map(link => <button className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl shadow-inner border-2 border-red-700 text-red-700 m-10 text-2xl p-2 rounded-2xl" style={{ fontFamily: "lobster" }}>  {link} </button>)}
             </div>
         </footer>
     )
--- portfolio/src/components/Languages.js
+++ portfolio/src/components/Languages.js
@@ -2,10 +2,11 @@ import { languages } from "../data/projects";
 
 function Languages() {
     return (
-        <article className="w-screen flex justify-center bg-red-700 min-h-1/2">
-            {languages.map(language => <button>{language}</button>)}
+        <article className="w-screen flex flex-wrap justify-center bg-red-700 ">
+            {languages.map(language => <button className="border-2 border-black m-10 text-6xl p-2 rounded-2xl" style={{ fontFamily: "lobster" }}>{language}</button>)}
         </article>
     )
 }
+
 
 export default Languages;
