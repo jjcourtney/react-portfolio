f3a7db7f07ef97c2d0affbeb8c6ed0a3d155c444
jjcourtney <jcourtneywork@gmail.com>
2021-12-11 00:28:41 +0000

    added flow for nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
+import Splash from "./components/Splash"
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Footer from './components/Footer';
+import Project from './components/Projects';
+import Languages from './components/Languages';
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("about")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black flex flex-col justify-between h-screen">
+      <Navbar setCurrentPage={setCurrentPage} />
+      <Splash />
+      {currentPage === "about" ? <About /> : ""}
+      {currentPage === "project" ? <Project /> : ""}
+      {currentPage === "languages" ? <Languages /> : ""}
+      <Footer className="bottom-0 absolute" />
     </div>
   );
 }
