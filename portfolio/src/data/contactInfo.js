f7bf1c3fd8a5774b3894b04a52b6cc120bee9b79
jjcourtney <jcourtneywork@gmail.com>
2021-12-11 19:29:31 +0000

    added array of links

---
+++ portfolio/src/data/contactInfo.js
@@ -0,0 +1,20 @@
+export const contactInfoArray = [
+    {
+        name: "linkedIn",
+        url: "https://www.linkedin.com/in/jon-courtney-20476370",
+        content: "https://www.linkedin.com/in/jon-courtney-20476370",
+        image: ""
+    },
+    {
+        name: "github",
+        url: "https://github.com/jjcourtney",
+        content: "https://github.com/jjcourtney",
+        image: ""
+    },
+    {
+        name: "email",
+        url: "mailto:jcourtneywork@gmail.com",
+        content: "jcourtneywork@gmail.com",
+        image: ""
+    }
+];
