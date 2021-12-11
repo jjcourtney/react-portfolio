445cfa2bce20581063e55ef3e164e7c83e25e8c2
jjcourtney <jcourtneywork@gmail.com>
2021-12-11 00:28:58 +0000

    updated css

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -4,11 +4,11 @@ function Navbar() {
     return (
         <>
             <nav className="w-screen bg-black flex flex-wrap justify-between align-middle">
-                <h1 className="text-2xl text-red-700 flex align- my-auto ml-3" style={{ fontFamily: 'Lobster' }}>
+                <h1 className="text-red-700 flex text-4xl my-auto ml-3" style={{ fontFamily: 'Lobster' }}>
                     Jonathan Courtney
                 </h1>
                 <div className="flex justify-end">
-                    {links.map(link => <button className="inline-block bg-red-700 hover:bg-red-400 text-white font-bold py-2 px-4 rounded m-5"> {link} </button>)}
+                    {links.map(link => <button className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl shadow-inner border-2 border-red-700 m-10 text-2xl p-2 rounded-2xl text-red-700" style={{ fontFamily: "lobster" }}> {link} </button>)}
                 </div>
             </nav>
         </>
