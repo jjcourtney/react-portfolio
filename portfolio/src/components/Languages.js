1236898c13fd49763327d310592a42f9994b3671
jjcourtney <jcourtneywork@gmail.com>
2021-12-11 00:28:24 +0000

    updated button css

--- portfolio/src/components/Languages.js
+++ portfolio/src/components/Languages.js
@@ -3,7 +3,7 @@ import { languages } from "../data/projects";
 function Languages() {
     return (
         <article className="w-screen flex flex-wrap justify-center bg-red-700 ">
-            {languages.map(language => <button className="border-2 border-black m-10 text-6xl p-2 rounded-2xl" style={{ fontFamily: "lobster" }}>{language}</button>)}
+            {languages.map(language => <button className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-2xl shadow-inner border-2 border-black m-10 text-6xl p-2 rounded-2xl" style={{ fontFamily: "lobster" }}>{language}</button>)}
         </article>
     )
 }
